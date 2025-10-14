99d7df5ca6bd44eb60d0c60c8a5fd0ea84a1033e
cedretaber <cedretaber@gmail.com>
2025-10-15 04:07:43 +0900

    AdmissibleDirs_sublist_rule を仮定して AdmissibleDirsStep_preserve を証明.

--- theories/AdmissibilityPreservation.v
+++ theories/AdmissibilityPreservation.v
@@ -1,8 +1,20 @@
 Require Import Admissible.
 Require Import Reduction.
 
+(* 補題: Ruleを部分リストに適用しても許容性が保たれる *)
+Axiom AdmissibleDirs_sublist_rule: forall l ds ds' r,
+  Rule ds ds' -> AdmissibleDirs (l ++ ds ++ r) -> AdmissibleDirs (l ++ ds' ++ r).
+
 Lemma AdmissibleDirsStep_preserve ds ds' : AdmissibleDirs ds -> ReduceDirStep ds ds' -> AdmissibleDirs ds'.
-Admitted.
+Proof.
+  intros AD RDS.
+  (* ReduceDirStep の定義から、l, r, es, es' と Rule es es' が存在する *)
+  inversion RDS as [l r es es' HRule Hds Hds'].
+  (* ds = l ++ es ++ r, ds' = l ++ es' ++ r である *)
+  subst ds ds'.
+  (* 補題を適用 *)
+  eapply AdmissibleDirs_sublist_rule; eauto.
+Qed.
 
 Lemma AdmissibleDirs_preserve ds ds': AdmissibleDirs ds -> ReduceDir ds ds' -> AdmissibleDirs ds'.
 Proof.
